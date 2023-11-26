2bbea7d557952a27a6996c57b0143a14c3a92fa7
Rifat Azad <33044977+rifsxd@users.noreply.github.com>
2023-11-26 12:05:13 +0600

    updated test code

--- src/kittyfetch_test.c
+++ src/kittyfetch_test.c
@@ -479,73 +479,13 @@ char *getGpuInfo() {
     return gpuInfo;
 }
 
-void minimal(int isVerbose, int isBunny) {
-    if (isVerbose) {
-        printf("\n");
-        if (isBunny) {
-            printf("\033[92m ( . .) Purr! This is bunnyfetch %s, your friendly system information display made with love by RifsxD! <3\033[0m\n\n", VERSION);
-        } else {
-            printf("\033[92m =>-<= Meow! This is kittyfetch %s, your friendly system information display made with love by RifsxD! <3\033[0m\n\n", VERSION);
-        }
-    }
-
-    if (isBunny) {
-        // Bunny printf
-        printf(
-            "            \n"
-            " \033[3m%s\n"
-            "            \n"
-            "            %s\n"
-            "            %s\n"
-            "   \033[38;5;15m(\\ /)\033[0m    %s\n"
-            "   \033[38;5;15m( . .)\033[0m   %s\n"
-            "   \033[38;5;15mc(\033[38;5;211m%c\033[\033[38;5;15m)(\033[38;5;211m%c\033[\033[38;5;15m)\033[0m  %s\n\n",
-            getRandomGreetingBunny(),
-            getTitleInfo(),
-            getOsInfo(),
-            getKernelInfo(),
-            getShellInfo(),
-            '"', '"',
-            getWmInfo()
-        );
-    } else {
-        // Default kitty printf
-        printf(
-            "            \n"
-            " \033[3m%s\n"
-            "            \n"
-            "            %s\n"
-            "   \033[38;5;94m/\\_/\\\033[0m    %s\n"
-            "  \033[38;5;15m( >.< )\033[0m   %s\n"
-            "   \033[38;5;94m= ^ =\033[0m    %s\n"
-            "  \033[38;5;15m~(\033[38;5;211m♥\033[\033[38;5;15m)(\033[38;5;211m♥\033[38;5;15m)   %s\n\n",
-            getRandomGreeting(),
-            getTitleInfo(),
-            getOsInfo(),
-            getKernelInfo(),
-            getShellInfo(),
-            getWmInfo()
-        );
-    }
-
-    printf("         ");
-    for (int i = 0; i < 8; i++) {
-        printf("\033[4%dm   ", i);
-    }
-    printf("\033[0m\n         ");
-    for (int i = 0; i < 8; i++) {
-        printf("\033[10%dm   ", i);
-    }
-    printf("\033[0m\n\n");
-}
-
 void extended(int isVerbose, int isBunny) {
     if (isVerbose) {
         printf("\n");
         if (isBunny) {
-            printf("\033[92m ( . .) Purr! This is bunnyfetch %s, your friendly system information display made with love by RifsxD! <3\033[0m\n\n", VERSION);
+            printf("\033[92m ( . .) Purr! This is bunny version %s, your friendly system information display made with love by RifsxD! <3\033[0m\n\n", VERSION);
         } else {
-            printf("\033[92m =>.<= Meow! This is kittyfetch %s, your friendly system information display made with love by RifsxD! <3\033[0m\n\n", VERSION);
+            printf("\033[92m =>.<= Meow! This is kitty version %s, your friendly system information display made with love by RifsxD! <3\033[0m\n\n", VERSION);
         }
     }
 
@@ -608,6 +548,66 @@ void extended(int isVerbose, int isBunny) {
             getGpuInfo(),
             getStorageInfo(),
             getRamInfo(),
+            getWmInfo()
+        );
+    }
+
+    printf("         ");
+    for (int i = 0; i < 8; i++) {
+        printf("\033[4%dm   ", i);
+    }
+    printf("\033[0m\n         ");
+    for (int i = 0; i < 8; i++) {
+        printf("\033[10%dm   ", i);
+    }
+    printf("\033[0m\n\n");
+}
+
+void minimal(int isVerbose, int isBunny) {
+    if (isVerbose) {
+        printf("\n");
+        if (isBunny) {
+            printf("\033[92m ( . .) Purr! This is bunny version %s, your friendly system information display made with love by RifsxD! <3\033[0m\n\n", VERSION);
+        } else {
+            printf("\033[92m =>-<= Meow! This is kitty version %s, your friendly system information display made with love by RifsxD! <3\033[0m\n\n", VERSION);
+        }
+    }
+
+    if (isBunny) {
+        // Bunny printf
+        printf(
+            "            \n"
+            " \033[3m%s\n"
+            "            \n"
+            "            %s\n"
+            "            %s\n"
+            "   \033[38;5;15m(\\ /)\033[0m    %s\n"
+            "   \033[38;5;15m( . .)\033[0m   %s\n"
+            "   \033[38;5;15mc(\033[38;5;211m%c\033[\033[38;5;15m)(\033[38;5;211m%c\033[\033[38;5;15m)\033[0m  %s\n\n",
+            getRandomGreetingBunny(),
+            getTitleInfo(),
+            getOsInfo(),
+            getKernelInfo(),
+            getShellInfo(),
+            '"', '"',
+            getWmInfo()
+        );
+    } else {
+        // Default kitty printf
+        printf(
+            "            \n"
+            " \033[3m%s\n"
+            "            \n"
+            "            %s\n"
+            "   \033[38;5;94m/\\_/\\\033[0m    %s\n"
+            "  \033[38;5;15m( >.< )\033[0m   %s\n"
+            "   \033[38;5;94m= ^ =\033[0m    %s\n"
+            "  \033[38;5;15m~(\033[38;5;211m♥\033[\033[38;5;15m)(\033[38;5;211m♥\033[38;5;15m)   %s\n\n",
+            getRandomGreeting(),
+            getTitleInfo(),
+            getOsInfo(),
+            getKernelInfo(),
+            getShellInfo(),
             getWmInfo()
         );
     }
